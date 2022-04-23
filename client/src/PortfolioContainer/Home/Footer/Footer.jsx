64aa269dd9bf97a2b8762e3051c8fea723b3c4fe
Ricardo Rocha Ribeiro <r3ifes@gmail.com>
2022-04-23 09:02:51 -0300

    header

---
+++ client/src/PortfolioContainer/Home/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export function Footer() {
+    return (
+        <div className="footer-container">
+            <div className='footer-parent'>
+                <img src={requestAnimationFrame('../../../assets/Home/shape-bg.png')} alt="no internet connection" />
+            </div>
+        </div>
+    )
+}
